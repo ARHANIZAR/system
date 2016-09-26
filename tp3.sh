981f35b113f5e9a2055c8259635aa539d257ae3f
root <root@tpreseau.tpreseau.org>
2016-09-26 19:02:43 +0200

    tp3

---
+++ tp3.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+dateDebut=$(date +%s)
+word=aymane
+timer=20
+read -p "temps restant $timer Entrez le mdp : " mdp
+
+while [ "$mdp"!=$word ] && [ $timer -gt 0 ]
+do
+	dateFin=$(date +%s)
+	let "timer = timer - dateFin + dateDebut"
+	read -p "temps restant $timer Entrez le mdp : " mdp
+done
+
+if [ $timer -gt 0 ]
+then
+	echo "Booom !!"
+elif [ "$mdp"==$word ]
+then
+	echo "Bravo !"
+else
+	echo "Err !!"
+fi
+
+
